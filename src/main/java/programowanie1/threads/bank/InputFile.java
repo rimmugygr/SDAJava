3848033f3705a3bfb068d82bb75ea2bac4f82778
rimmugyr <rimmugygr@gmail.com>
2020-02-09 12:19:28 +0100

    commit IO not done

--- src/main/java/programowanie1/threads/bank/InputFile.java
+++ src/main/java/programowanie1/threads/bank/InputFile.java
@@ -5,24 +5,23 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InputFile {
+    private static Map<String,String> dataFileMap = new HashMap<>();
 
+    public static final Map reaWithBufferedReader(String nameFile){
 
-    public static final Map<Integer,List<String>> reaWithBufferedReader(String nameFile){
-        Map<Integer,List<String>> dataFileMap = new HashMap<>();
 
         try(BufferedReader reader = new BufferedReader(new FileReader(nameFile))){
-
-            Integer numberLine=0;
             String input;
             String filedSepartor = "|";
             while ((input=reader.readLine())!=null){//wczytuje puki jest następna linia
-                List<String> listLine = new ArrayList<String>(Arrays.asList(input.split(filedSepartor)));//dzelimy linie na pola
-                dataFileMap.put(numberLine++,listLine);
-            }
+                String[] dataLine=input.split(filedSepartor);//dzelimy linie na pola
 
+            }
+            return dataFileMap;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
