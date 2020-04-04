1c0332a5dae524eebc9bec0cc590b2d3e19e6c0a
rimmugyr <rimmugygr@gmail.com>
2020-04-04 14:22:57 +0200

    programing 2 second task

--- src/main/java/programing2/Wallet/Person.java
+++ src/main/java/programing2/Wallet/Person.java
@@ -1,5 +1,36 @@
 package programing2.Wallet;
 
 public class Person {
+    private String firstName;
+    private String lastName;
     private Wallet wallet;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
+    }
 }
--- src/main/java/programing2/Wallet/Wallet.java
+++ src/main/java/programing2/Wallet/Wallet.java
@@ -1,7 +1,23 @@
 package programing2.Wallet;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Wallet {
     private List<Cash> cash;
+    
+    {
+        this.cash = new ArrayList<>();
+    }
+
+    public void addCash(BigDecimal money, Currency currency) {
+        for (Cash c : cash) {
+            
+        }
+        
+        this.cash.add(new Cash(money,currency));
+        
+        
+    }
 }
